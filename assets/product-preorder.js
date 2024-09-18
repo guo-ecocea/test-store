03c383c5d6af5284eef26327e479be5304a48c83
Guo YU <gyu@ecocealafabrique.com>
2024-09-18 17:07:32 +0200

    fix: display preorder after changing modal bug

--- assets/product-preorder.js
+++ assets/product-preorder.js
@@ -43,115 +43,118 @@
 <script src="{{ 'product-preorder.js' | asset_url }}" defer></script>
 */
 
-const variants = JSON.parse(
-  document.querySelector('script[data-ref="variant-ids"]').textContent
-);
-const data = JSON.parse(
-  document.querySelector('script[data-ref="product-datas"]').textContent
-);
-let localText = data.localText;
-let product = data.product;
-let variantDict = {};
-let preorders = {};
-
-variants.forEach((variant) => {
-  variantDict[variant.id] = variant;
-});
-
-function variantSelectListener() {
-  let parentElement = document.querySelector(
-    `section[id="ProductInfo-${data.sectionId}"]`
+(function () {
+  const variants = JSON.parse(
+    document.querySelector('script[data-ref="variant-ids"]').textContent
   );
-  console.log(data.sectionId);
+  const data = JSON.parse(
+    document.querySelector('script[data-ref="product-datas"]').textContent
+  );
+  let localText = data.localText;
+  let product = data.product;
+  let variantDict = {};
+  let preorders = {};
 
-  if (parentElement) {
-    parentElement.addEventListener("change", function (event) {
-      data.options_ids.forEach((id) => {
-        if (event.target && event.target.matches(`input[id="${id}"]`)) {
-          const selectedVariant = variants.find(
-            (el) => el.title == event.target.value
-          );
-          displayVariantPreorder(selectedVariant.id);
-        }
+  variants.forEach((variant) => {
+    variantDict[variant.id] = variant;
+  });
+
+  function variantSelectListener() {
+    let parentElement = document.querySelector(
+      `section[id="ProductInfo-${data.sectionId}"]`
+    );
+
+    if (parentElement) {
+      parentElement.addEventListener("change", function (event) {
+        data.options_ids.forEach((id) => {
+          if (event.target && event.target.matches(`input[id="${id}"]`)) {
+            const selectedVariant = variants.find(
+              (el) => el.title == event.target.value
+            );
+            displayVariantPreorder(selectedVariant.id);
+          }
+        });
       });
-    });
-  }
-}
-
-variantSelectListener();
-
-function fillPreorders() {
-  for (let i = 0; i < variants.length; i++) {
-    if (
-      variants[i].pre_order.length > 0 &&
-      Date.parse(variants[i].pre_order) > Date.now()
-    )
-      preorders[variants[i].id] = {
-        value: variants[i].pre_order,
-        pre_order_formatted_date: variants[i].pre_order_formatted_date,
-      };
-  }
-}
-
-fillPreorders();
-
-function getQueryParam(param) {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get(param);
-}
-
-function createPreorderTag(variantId) {
-  if (variantDict[variantId].available && variantId in preorders) {
-    const h3 = document.getElementById("variant-preorder-h3");
-    h3.innerText = `${localText.pre_order.pre_ordable_for_the} ${preorders[variantId].pre_order_formatted_date}`;
-    let label = document.getElementById("preorder-label");
-    const p = document.getElementById("variant-preorder-p");
-    if (!label) {
-      label = document.createElement("label");
-      label.setAttribute("id", "preorder-label");
-      label.setAttribute("for", "variant-delivery-date");
-      label.innerText = `${localText.pre_order.pre_order_delivery_date}`;
-      p.appendChild(label);
     }
-    let input = document.getElementById("variant-delivery-date");
-    if (!input) {
-      input = document.createElement("input");
-      input.setAttribute("id", "variant-delivery-date");
-      input.setAttribute("type", "text");
+  }
+
+  variantSelectListener();
+
+  function fillPreorders() {
+    for (let i = 0; i < variants.length; i++) {
+      if (
+        variants[i].pre_order.length > 0 &&
+        Date.parse(variants[i].pre_order) > Date.now()
+      )
+        preorders[variants[i].id] = {
+          value: variants[i].pre_order,
+          pre_order_formatted_date: variants[i].pre_order_formatted_date,
+        };
+    }
+  }
+
+  fillPreorders();
+
+  function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+  }
+
+  function createPreorderTag(variantId) {
+    if (variantDict[variantId].available && variantId in preorders) {
+      const h3 = document.getElementById("variant-preorder-h3");
+      h3.innerText = `${localText.pre_order.pre_ordable_for_the} ${preorders[variantId].pre_order_formatted_date}`;
+      let label = document.getElementById("preorder-label");
+      const p = document.getElementById("variant-preorder-p");
+      if (!label) {
+        label = document.createElement("label");
+        label.setAttribute("id", "preorder-label");
+        label.setAttribute("for", "variant-delivery-date");
+        label.innerText = `${localText.pre_order.pre_order_delivery_date}`;
+        p.appendChild(label);
+      }
+      let input = document.getElementById("variant-delivery-date");
+      if (!input) {
+        input = document.createElement("input");
+        input.setAttribute("id", "variant-delivery-date");
+        input.setAttribute("type", "text");
+        input.setAttribute(
+          "name",
+          `properties[${localText.pre_order.pre_order_delivery_date}]`
+        );
+        input.setAttribute("form", data.productFormId);
+        p.appendChild(input);
+      }
       input.setAttribute(
-        "name",
-        `properties[${localText.pre_order.pre_order_delivery_date}]`
+        "value",
+        preorders[variantId].pre_order_formatted_date
       );
-      input.setAttribute("form", data.productFormId);
-      p.appendChild(input);
+    } else {
+      const h3 = document.getElementById("variant-preorder-h3");
+      if (h3) h3.innerText = "";
+      const label = document.getElementById("preorder-label");
+      if (label) label.remove();
+      const input = document.getElementById("variant-delivery-date");
+      if (input) input.remove();
     }
-    input.setAttribute("value", preorders[variantId].pre_order_formatted_date);
-  } else {
-    const h3 = document.getElementById("variant-preorder-h3");
-    if (h3) h3.innerText = "";
-    const label = document.getElementById("preorder-label");
-    if (label) label.remove();
-    const input = document.getElementById("variant-delivery-date");
-    if (input) input.remove();
   }
-}
 
-function displayVariantPreorder(variantId) {
-  if (variantId && variantDict[variantId]) {
-    createPreorderTag(variantId);
-  } else if (product.variants && product.variants.length > 0) {
-    const selectedVariant = data.selected_or_first_available_variant;
-    createPreorderTag(selectedVariant.id);
-  } else {
-    console.log("No variant parameter in URL or not preorder.");
-    console.log(product);
+  function displayVariantPreorder(variantId) {
+    if (variantId && variantDict[variantId]) {
+      createPreorderTag(variantId);
+    } else if (product.variants && product.variants.length > 0) {
+      const selectedVariant = data.selected_or_first_available_variant;
+      createPreorderTag(selectedVariant.id);
+    } else {
+      console.log("No variant parameter in URL or not preorder.");
+    }
   }
-}
 
-function checkVariantPreorder() {
-  // console.log(data);
-  const variantId = getQueryParam("variant");
-  displayVariantPreorder(variantId);
-}
+  function checkVariantPreorder() {
+    // console.log(data);
+    const variantId = getQueryParam("variant");
+    displayVariantPreorder(variantId);
+  }
 
-checkVariantPreorder();
+  checkVariantPreorder();
+})();
